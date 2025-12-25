91d16d4b45c8ecd4a9fecedef2cd7407834b24fc
hi1587 <darboy16651@gmail.com>
2025-12-25 14:51:45 +0100

    linoria test

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/hi1587/hi1587-doors-admin-panel/releases/download/0.0.1/Main.lua"))()
+loadstring(game:HttpGet("https://github.com/hi1587/hi1587-doors-admin-panel/releases/download/0.0.2/Main.lua"))()
